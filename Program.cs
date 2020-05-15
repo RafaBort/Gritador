77c48fac82546396f3f5469c900b6f237c14f015
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-05-15 19:41:11 -0300

    exercicíos resolvidos

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite algo!");
+
+            string texto;
+            texto = Console.ReadLine();
+
+            Console.WriteLine(texto.ToUpper());
+        }
+    }
+}
